3e68e986e8ce2ec0d25a7ec5a03f0b9850ab047e
Tomasz Naglik <naglikt@gmail.com>
2023-01-24 16:40:49 +0100

    Hello World

--- Assets/Scripts/Map.cs
+++ Assets/Scripts/Map.cs
@@ -7,7 +7,7 @@ public class Map : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Hello world");
     }
 
     // Update is called once per frame
